502a034ee536e436313d9f7c5d94e5febdbed618
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-09-01 18:18:54 +0530

    Working

--- client/index.js
+++ client/index.js
@@ -1,1 +1,21 @@
-console.log("Start the project from here")
+const { response } = require("express")
+
+const tableBody = document.getElementById('table-body')
+
+const getFlight = () => {
+    fetch('http://localhost:8000/flights')
+    .then(response => response.json())
+    .then(flights => {
+        populateTable(flights)
+    })
+    .catch(err => console.log(err))
+}
+
+const populateTable = (flights) => {
+    for (const flight of flights) {}
+    const tableRow = document.createElement('tr')
+    const tableIcon = document.createElement('td')
+    tableIcon.textContent = "✈"
+    tableRow.append(tableIcon)
+    tableBody.append(tableRow)
+}
