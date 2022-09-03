f3e110222b406ad01ad3ec7a2452e12d6629a7b7
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-09-04 02:56:16 +0530

    Completed

--- client/index.js
+++ client/index.js
@@ -1,5 +1,3 @@
-const { response } = require("express")
-
 const tableBody = document.getElementById('table-body')
 
 const getFlight = () => {
@@ -10,6 +8,7 @@ const getFlight = () => {
     })
     .catch(err => console.log(err))
 }
+getFlight()
 
 const populateTable = (flights) => {
     for (const flight of flights) {
@@ -17,5 +16,33 @@ const populateTable = (flights) => {
     const tableIcon = document.createElement('td')
     tableIcon.textContent = "✈"
     tableRow.append(tableIcon)
-    tableBody.append(tableRow)}
+    
+    // tableBody.append(tableRow)
+
+    const flightDetails = {
+        time: flight.departing.slice(0,5),
+        destination: flight.destination.toUpperCase(),
+        flight: flight.flightNumber.shift(),
+        gate: flight.gate,
+        remarks: flight.status.toUpperCase()
+    }
+    for(const flightDetail in flightDetails){
+        const tableCell = document.createElement('td')
+        const word = Array.from(flightDetails[flightDetail])
+
+        for(const [index, letter] of word.entries()){
+            const letterElement = document.createElement('div')
+
+            setTimeout(()=>{
+                letterElement.classList.add('flip')
+                letterElement.textContent = letter;
+                tableCell.append(letterElement)
+            }, 100 * index)
+            
+        }
+        tableRow.append(tableCell)
+
+    }
+    tableBody.append(tableRow)
+}
 }
--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const PORT = 8000
-const axios = require('axios').default
-const express = require('express')
+const PORT = 8000;
+const axios = require("axios").default;
+const express = require("express");
 const app = express();
-const cors = require('cors')
-require('dotenv').config()
-app.use(cors())
+const cors = require("cors");
+require("dotenv").config();
+app.use(cors());
 
-app.listen(PORT, () => console.log('running on PORT ' + PORT))
+app.listen(PORT, () => console.log("The Server is running on PORT " + PORT));
 
-app.get('/flights', (req, res) => {
-    const options = {
-        method: 'GET',
-        url: 'https://toronto-pearson-airport.p.rapidapi.com/arrivals',
-        headers: {
-          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-          'X-RapidAPI-Host': 'toronto-pearson-airport.p.rapidapi.com'
-        }
-      };
-      
-      axios.request(options).then(function (response) {
-          console.log(response.data);
-          res.json(response.data.slice(0,6))
-      }).catch(function (error) {
-          console.error(error);
-      });
-})
+app.get("/flights", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://toronto-pearson-airport.p.rapidapi.com/departures",
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": "toronto-pearson-airport.p.rapidapi.com",
+    },
+  };
 
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      res.json(response.data.slice(0, 6));
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
